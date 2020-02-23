3c5104c0cfa7b6a4050c76c55d892d8c2c7eb9c9
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-02-23 23:33:58 +0530

    explicitly make a unique index for user_id-movie_id combination

--- database/migrations/2020_02_23_171943_create_movie_user_table.php
+++ database/migrations/2020_02_23_171943_create_movie_user_table.php
@@ -23,7 +23,7 @@ class CreateMovieUserTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
 
-            $table->index(['user_id', 'movie_id']); // so that there's only one (movie, user) combination
+            $table->unique(['user_id', 'movie_id']); // so that there's only one (movie, user) combination
         });
     }
 
