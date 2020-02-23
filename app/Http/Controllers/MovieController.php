d3d4f17b23c0352404ee480cba316b86352e9e85
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-02-23 21:45:50 +0530

    show list of movies sorted by title in /movies, along with their release date

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,7 +14,9 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        return view('movie.index', [
+            'movies' => Movie::orderBy('title', 'asc')->get(),
+        ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'UserController@index');
+Route::get('/movies', 'MovieController@index');
 // Route::get('/home', 'HomeController@index')->name('home');
